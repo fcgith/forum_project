f9ecab50d3a8bd25f30925655ac1ecc4facfcf88
fcgith <steliyan.slavov31@icloud.com>
2025-04-05 22:27:32 +0300

    Hiding credentials

--- db.py
+++ db.py
@@ -1,8 +1,17 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:1234!@172.245.56.116:65124/forum_project"
+SISO_FORUM_DB_USER = os.getenv("SISO_FORUM_DB_USER")
+SISO_FORUM_DB_PASSWORD = os.getenv("SISO_FORUM_DB_PASSWORD")
+SISO_FORUM_DB_HOST = os.getenv("SISO_FORUM_DB_HOST")
+SISO_FORUM_DB_PORT = os.getenv("SISO_FORUM_DB_PORT")
+SISO_FORUM_DB_NAME = os.getenv("SISO_FORUM_DB_NAME")
+
+SQLALCHEMY_DATABASE_URL =\
+    f"postgresql+psycopg2://{SISO_FORUM_DB_USER}:{SISO_FORUM_DB_PASSWORD}@{SISO_FORUM_DB_HOST}:{SISO_FORUM_DB_PORT}/{SISO_FORUM_DB_NAME}"
 
 engine = create_engine\
 (
