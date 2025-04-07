c9ef12b7d765c1cd83cd5a2c3a1ebb1dd8430096
fcgith <steliyan.slavov31@icloud.com>
2025-04-07 12:44:13 +0300

    fixed categories / added topics

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 from auth import router as auth_router
 from admin import router as admin_router
 from categories import router as category_router
+from topics import router as topics_router
 
 app = FastAPI()
 
 app.include_router(auth_router, prefix="/auth")
 app.include_router(admin_router, prefix="/admin")
 app.include_router(category_router, prefix="/categories")
-app.include_router(category_router, prefix="/topics")
+app.include_router(topics_router, prefix="/topics")
 
 if __name__ == "__main__":
     import uvicorn
